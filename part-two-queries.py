1239bcb3418f4793bc94e80d48a55593e060d12e
elikkozak <elik.kozak@gmail.com>
2022-10-14 00:30:30 +0300

    FINISHED EX2

--- part-two-queries.py
+++ part-two-queries.py
@@ -21,5 +21,19 @@ def get_heaviest_pokemon():
     except TypeError as e:
         return e
 
+#   EX2
+
+def find_pokemons_by_type(type):
+    try:
+        with connection.cursor() as cursor:
+            query = f'SELECT pokemon.p_name FROM pokemon WHERE pokemon.type = "{type}"'
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return list(map(lambda p_name_dict:p_name_dict["p_name"],result))
+    except TypeError as e:
+        return e
+    pass
+
 if __name__ == "__main__":
-    print(get_heaviest_pokemon())
+    # print(get_heaviest_pokemon())
+    print(find_pokemons_by_type("grass"))
