c7226c580c3ecf8f7a6e2fb2b9638ca35981ceed
elikkozak <elik.kozak@gmail.com>
2022-10-14 14:36:53 +0300

    FINISHED EX 4

--- part-two-queries.py
+++ part-two-queries.py
@@ -41,10 +41,11 @@ def find_pokemons_by_type(type):
 def find_pokemon_owners(pokemon_name):
     try:
         with connection.cursor() as cursor:
-            query = f'''SELECT pt.t_name FROM pokemon_trainer AS pt
-             JOIN pokemon
-             ON (SELECT p.id FROM pokemon AS p WHERE p.p_name = "{pokemon_name}") = pt.p_id
-             WHERE pokemon.p_name = "{pokemon_name}"'''
+            query = f'''SELECT pt.t_name 
+                        FROM pokemon_trainer AS pt
+                        JOIN pokemon
+                        ON (SELECT p.id FROM pokemon AS p WHERE p.p_name = "{pokemon_name}") = pt.p_id
+                        WHERE pokemon.p_name = "{pokemon_name}"'''
             cursor.execute(query)
             result = cursor.fetchall()
             return list(map(lambda t_name_dict: t_name_dict["t_name"], result))
@@ -52,7 +53,25 @@ def find_pokemon_owners(pokemon_name):
         return e
 
 
+#   EX4
+
+def find_pokemon_roster(trainer_name):
+    try:
+        with connection.cursor() as cursor:
+            query = f'''SELECT p.p_name
+                        FROM pokemon AS p 
+                        JOIN pokemon_trainer AS pt
+                        ON p.id = pt.p_id
+                        WHERE pt.t_name = "{trainer_name}"'''
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return list(map(lambda p_name_dict: p_name_dict["p_name"], result))
+    except TypeError as e:
+        return e
+
+
 if __name__ == "__main__":
     # print(get_heaviest_pokemon())
     # print(find_pokemons_by_type("grass"))
-    print(find_pokemon_owners("exeggutor"))
+    # print(find_pokemon_owners("gengar"))
+    print(find_pokemon_roster("Loga"))
