9e1c31f90e504455f1aa532288ae949166b86c66
YaelNorman1 <YaelNorman2808@gmail.com>
2022-10-16 12:03:18 +0300

    returning the pokemon to user with all data saved

--- part-three-server.py
+++ part-three-server.py
@@ -16,12 +16,26 @@ connection = pymysql.connect(
 
 
 
-@app.get("/pokemon/types", status_code=status.HTTP_200_OK)
-def get_type_pokemon(pokemon_name):
+@app.get("/pokemon", status_code=status.HTTP_200_OK)
+def get_pokemon(pokemon_name):
     req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
-    pokemon_id= get_pokemon_id(pokemon_name)
-    save_types_in_tabels(pokemon_id, req.json())
+    pokemon_data = get_pokemon_data(pokemon_name)
+    pokemon_id= pokemon_data["id"]
+    save_types_in_tabels(pokemon_id, req.json()["types"])
+    pokemon_types_dict = get_pokemon_types(pokemon_id)
+    pokemon_types_lst = list(map(lambda pokemon_type:pokemon_type["p_type"],pokemon_types_dict))
+    pokemon_data["types"] = pokemon_types_lst
+    return pokemon_data
 
+def get_pokemon_data(pokemon_name):
+    try:
+        with connection.cursor() as cursor:
+            query = f'SELECT * FROM pokemon WHERE p_name = "{pokemon_name}";'
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result
+    except TypeError as e:
+        return e
 
 def get_pokemon_id(name):
     try:
@@ -33,9 +47,20 @@ def get_pokemon_id(name):
     except TypeError as e:
         return e
 
+
+def get_pokemon_types(pokemon_id):
+    try:
+        with connection.cursor() as cursor:
+            query = f'SELECT p_type FROM pokemon_type WHERE p_id = "{pokemon_id}";'
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result
+    except TypeError as e:
+        return e
+
 def save_types_in_tabels(pokemon_id, pokemon_types):
-    for type in pokemon_types["types"]:
-        insert_each_type(pokemon_id["id"], type["type"]["name"])
+    for type in pokemon_types:
+        insert_each_type(pokemon_id, type["type"]["name"])
 
 
 def insert_each_type(id, type):
